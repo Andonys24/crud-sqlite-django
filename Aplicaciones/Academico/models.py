c7ddee7581d398416ba0686fc602109073967abe
Andonys24 <andonihernandez1990@gmail.com>
2024-07-05 19:45:42 -0600

    Creacion de templates y Vistas - Creacion de la ruta de gestionCursos.html - Creacion de Vista Home en Views - Trajendo los datos de la base de datos del modelo Curso a la Vista - Creacion de configuracion de rutas en:  Aplicaciones/Academico/urls.py

--- Aplicaciones/Academico/models.py
+++ Aplicaciones/Academico/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
 class Curso(models.Model):
     codigo = models.CharField(primary_key=True, max_length=6)
     nombre = models.CharField(max_length=60)
-    credenciales = models.PositiveSmallIntegerField()
+    creditos = models.PositiveSmallIntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.nombre} ({self.creditos})"
