a4d84e2165add289c04317f8e7db2b7e0908c002
Andonys24 <andonihernandez1990@gmail.com>
2024-07-05 21:15:28 -0600

    Actualizacion de Cursos
    * Nueva funcion para actualizar un curso
    - Nueva url implementada para enviarnos a la pagina
        de edicionCurso.html donde podremos cambiar los datos del curso
    
    * Mejora en el formulario de creacion de Curso
    - ocultamiento del input de txtCodigo
    - Campo de txtCodigo se rellena automaticamente
    - Haciendo mas facil la creacion de nuevos cursos

--- Aplicaciones/Academico/views.py
+++ Aplicaciones/Academico/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Curso
+import random
 
 
 # Create your views here.
@@ -9,11 +10,41 @@ def home(request):
 
 
 def registrarCurso(request):
+    if request.method == "POST":
+        # Obtener los datos del formulario
+        nombre = request.POST.get("txtNombre")
+        creditos = request.POST.get("numCreditos")
+
+        # Generar el código aleatorio de 6 dígitos
+        random_codigo = "".join(random.choices("0123456789", k=6))
+
+        # Crear el curso con el código aleatorio y otros datos del formulario
+        curso = Curso.objects.create(
+            codigo=random_codigo, nombre=nombre, creditos=creditos
+        )
+        return redirect("/")
+    else:
+        # Generar número aleatorio de 6 dígitos
+        random_codigo = "".join(random.choices("0123456789", k=6))
+
+        # Renderizar el formulario con el número aleatorio prellenado
+        return render(request, "gestionCursos.html", {"random_codigo": random_codigo})
+
+
+def edicionCurso(request, codigo):
+    curso = Curso.objects.get(codigo=codigo)
+    return render(request, "edicionCurso.html", {"curso": curso})
+
+
+def editarCurso(request):
     codigo = request.POST["txtCodigo"]
     nombre = request.POST["txtNombre"]
     creditos = request.POST["numCreditos"]
 
-    curso = Curso.objects.create(codigo=codigo, nombre=nombre, creditos=creditos)
+    curso = Curso.objects.get(codigo=codigo)
+    curso.nombre = nombre
+    curso.creditos = creditos
+    curso.save()
     return redirect("/")
 
 
